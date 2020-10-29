06666a6526f098789e30bd2e26d5d4ba4c02cf09
Fdeleval <48656440+Fdeleval@users.noreply.github.com>
2020-10-29 14:15:47 +0100

    Add files via upload

---
+++ api.py
@@ -0,0 +1,24 @@
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+def testOutput(age):
+  return jsonify([{'name': 'fred', 'age': age}])
+
+
+@app.route('/', methods=['GET'])
+def home():
+  return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+@app.route('/test', methods = ['GET', 'POST'])
+def api_test_get():
+  print("=====")
+  print(request.method)
+  print(request.data)
+  print(request.get_json())
+  print("$$$$$$    $$$$$$$$")
+  return jsonify(request.args)
+
+app.run()
