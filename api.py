b809ec6bc24b45fd0a4370c701ed8ca6a37f5934
Fdeleval <48656440+Fdeleval@users.noreply.github.com>
2020-10-29 15:27:10 +0100

    Update api.py

--- api.py
+++ api.py
@@ -7,10 +7,6 @@ app.config["DEBUG"] = True
 def testOutput(age):
   return jsonify([{'name': 'fred', 'age': age}])
 
-@app.route('/favicon.ico')
-def fav()
-  return "<h1>What is this?</h1>"
-
 @app.route('/', methods=['GET'])
 def home():
   return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
