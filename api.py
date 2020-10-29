28b8c92b1d878aa65e20c2db347f0d995824cbc0
FDeleval <frederic.deleval@student.uclouvain.be>
2020-10-29 15:35:35 +0100

    adding a search for movie

--- api.py
+++ api.py
@@ -25,6 +25,7 @@ def home():
 @app.route('/test', methods = ['GET', 'POST'])
 def api_test_get():
   res = request.args
+  print(res.body)
   movie = res.to_dict(flat=False)['pos'][0]
   print(movie)
   return testOutput(movie)
