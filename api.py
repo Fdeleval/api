3977dbe223712ba69200f95c785d3a69104fa7a6
FDeleval <frederic.deleval@student.uclouvain.be>
2020-10-31 15:27:39 +0100

    update on pyodbc

--- api.py
+++ api.py
@@ -41,16 +41,6 @@ def db_get_artists():
 
   return build_actual_response(jsonify(result))
 
-@app.route('/uningo', methods = ['GET'])
-def db_get_artists():
-  result = []
-  arr = cursor.execute("SELECT * FROM stblcountry")
-  for row in arr:
-    result.append({row[0]: row[1]})
-
-  return build_actual_response(jsonify(result))
-
-
 # def build_preflight_response():
 #     response = make_response()
 #     response.headers.add("Access-Control-Allow-Origin", "*")
